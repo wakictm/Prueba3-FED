d7bfc4c6631d9287ff418513cef7b6da11ad4efd
Joaquín Wylie <joaco.andreswylie@gmail.com>
2026-06-17 19:51:30 -0400

    iniciando proyecto

---
+++ Prueba3-Grupo02/src/main/java/com/mycompany/prueba3/grupo02/Prueba3Grupo02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba3.grupo02;
+
+/**
+ *
+ * @author wakictm
+ */
+public class Prueba3Grupo02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
